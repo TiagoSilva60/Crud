e1de8b71f96ae39517a9d7445609e8276d61d176
Rudarb <68770247+TiagoSilva60@users.noreply.github.com>
2023-09-21 11:21:32 -0300

    Crud4

--- _header.php
+++ _header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style.css?t=<?php echo date('YmdHis').rand(1,9999);?>">
-    <title>CRUD</title>
+    <title>Crud Tiago</title>
 </head>
 <body>
