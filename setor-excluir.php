40345dc5293fa56552a083dde57981f4349532e4
Rudarb <68770247+TiagoSilva60@users.noreply.github.com>
2023-09-21 11:20:45 -0300

    Crud3

--- setor-excluir.php
+++ setor-excluir.php
@@ -2,11 +2,11 @@
 include_once './_conexao.php';
 
 
-// consulta
+
 $sql = "DELETE FROM setor WHERE SetorID = ".$_GET['id'];
 mysqli_query($conexao,$sql);
 
-// faz o redirecionamento
+
 header("Location: ./setor-lista.php", true, 302);
 exit();
 ?>
